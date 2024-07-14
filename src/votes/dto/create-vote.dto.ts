c4b0c742ecda19e75448943a2de3b4e23bc1b8ae
이영우 <zestlee1106@gmail.com>
2024-07-14 16:50:02 +0900

    update: class validator 가 검사할 수 있게끔 dto 에 각 타입을 데코레이터로 넣어줌
    - 객체 배열은 Nested 로 추가해 줌

--- src/votes/dto/create-vote.dto.ts
+++ src/votes/dto/create-vote.dto.ts
@@ -1,12 +1,48 @@
+import { Type } from 'class-transformer';
+import {
+  IsDate,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+class Result {
+  @IsNumber()
+  value: number;
+}
+
 export class CreateVoteDto {
+  @IsString()
   readonly title: string;
+
+  @IsOptional()
+  @IsString()
   readonly description: string;
+
+  @IsString({ each: true })
   readonly options: string[];
+
+  @IsDate()
+  @Type(() => Date) // transformer 가 string 을 Date 객체로 변환해 준다
   readonly startDate: Date;
+
+  @IsDate()
   readonly endDate: Date;
+
+  @IsDate()
   readonly createAt: Date;
+
+  @IsDate()
   readonly updatedAt: Date;
+
+  @IsString({ each: true })
   readonly votedIps: string[];
+
+  @IsString({ each: true })
   readonly votedCookieIds: string[];
-  readonly results: { [key: string]: number };
+
+  @ValidateNested()
+  @Type(() => Result)
+  readonly results: Record<string, number>;
 }
