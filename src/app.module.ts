09ab6e71b2a3762485ee111b44d98606ad19444c
이영우 <zestlee1106@gmail.com>
2024-07-14 16:09:03 +0900

    update: app module 에 typeorm 모듈을 써서 로컬 mongodb 를 연결함

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { VotesModule } from './votes/votes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [VotesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb', // MongoDB 데이터베이스 사용
+      url: 'mongodb://localhost/nest', // MongoDB 연결 URL (로컬호스트에 nest 데이터베이스 사용)
+      useUnifiedTopology: true, // 새로운 MongoDB 드라이버의 통합 토폴로지 사용
+      synchronize: true, // 애플리케이션 시작 시 데이터베이스 스키마 자동 동기화 (프로덕션에서는 사용 안 하는게 좋음)
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // 엔티티 파일 경로 지정
+    }),
+    VotesModule,
+  ],
   controllers: [],
   providers: [],
 })
