529497b842fc088843eddc9728ff7be4d928e5e7
이영우 <zestlee1106@gmail.com>
2024-07-14 16:39:41 +0900

    refactor: 컨벤션에 맞춰서 네이밍 수정

--- src/votes/votes.service.ts
+++ src/votes/votes.service.ts
@@ -9,13 +9,13 @@ export class VotesService {
   constructor(
     // 의존성 주입을 위해 생성자 추가
     @InjectRepository(Vote) // Vote 엔티티와 연동된 리포지토리를 주입하기 위해 사용
-    private voteRepository: Repository<Vote>, // Repository<Vote> 가 Vote 엔티티와 관련된 DB 작업을 수행하는 메서드를 포함함
+    private votesRepository: Repository<Vote>, // Repository<Vote> 가 Vote 엔티티와 관련된 DB 작업을 수행하는 메서드를 포함함
   ) {}
 
   async create(createVoteDto: CreateVoteDto): Promise<Vote> {
     // 1. DTO를 엔티티 인스턴스로 변환
-    const vote = this.voteRepository.create(createVoteDto);
+    const vote = this.votesRepository.create(createVoteDto);
     // 2. 엔티티 인스턴스를 데이터베이스에 저장
-    return this.voteRepository.save(vote);
+    return this.votesRepository.save(vote);
   }
 }
