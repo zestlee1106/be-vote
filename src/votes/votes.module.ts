5d1fc445e0094401efc43844f814e167d29790ba
이영우 <zestlee1106@gmail.com>
2024-07-14 16:32:47 +0900

    add: vote 모듈에 Vote typeorm 모듈 추가 (service 에서 DI 를 위해 추가해 주는 것)

--- src/votes/votes.module.ts
+++ src/votes/votes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VotesController } from './votes.controller';
 import { VotesService } from './votes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Vote } from './entities/vote.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Vote])],
   controllers: [VotesController],
   providers: [VotesService],
 })
