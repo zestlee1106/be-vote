0e8f87097613708322788da18e220ed3106624b6
이영우 <zestlee1106@gmail.com>
2024-07-14 16:18:01 +0900

    update: typeorm 을 써서 vote 엔티티 업데이트
    - Mongo DB 와 typeorm 을 같이 사용하면 typescript 타입을 기반으로 자동 추론해서 배열을 저장해 준다
    - 객체도 그렇게 자동으로 추론이 되지만, 문자열로 저장이 될 수 있기 때문에 'simple-json' 이라고 명시적으로 넣어줘야 한다
    - simple 하지 않은 복잡한 json 일 땐 jsonb 를 넣어주면 된다

--- src/votes/entities/vote.entity.ts
+++ src/votes/entities/vote.entity.ts
@@ -1,13 +1,37 @@
+import { Column, Entity, ObjectIdColumn } from 'typeorm';
+
+@Entity()
 export class Vote {
+  @ObjectIdColumn()
   id: number;
+
+  @Column()
   title: string;
+
+  @Column()
   description: string;
+
+  @Column()
   options: string[];
+
+  @Column()
   startDate: Date;
+
+  @Column()
   endDate: Date;
+
+  @Column()
   createAt: Date;
+
+  @Column()
   updatedAt: Date;
+
+  @Column()
   votedIps: string[];
+
+  @Column()
   votedCookieIds: string[];
+
+  @Column('simple-json')
   results: { [key: string]: number }; // 각 옵션에 대한 투표 수 저장
 }
