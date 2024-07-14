743449fee74ecd7c328ec91d448507d0aa72df5e
이영우 <zestlee1106@gmail.com>
2024-07-14 16:49:26 +0900

    add: global pipe 추가 (약간 미들웨어처럼 요청마다 얘가 검사해 줌)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO 에 선언되지 않은 것들은 whitelist 에 넣어줌
+      forbidNonWhitelisted: true, // whitelist 에 없는 것들은 400 대 에러 내려줌
+      transform: true, // type 을 바꿔준다 (예: qs 는 string 인데 number 로)
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
