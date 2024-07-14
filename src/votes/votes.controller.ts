a2dd56c1c46fd5c6cfb494185f9d72f2941d85c1
이영우 <zestlee1106@gmail.com>
2024-07-14 16:40:49 +0900

    update: vote 컨트롤러에 vote service DI 추가 및 create 추가

--- src/votes/votes.controller.ts
+++ src/votes/votes.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { VotesService } from './votes.service';
+import { CreateVoteDto } from './dto/create-vote.dto';
+import { Vote } from './entities/vote.entity';
 
 @Controller('votes')
-export class VotesController {}
+export class VotesController {
+  constructor(private readonly votesService: VotesService) {}
+
+  @Post()
+  async create(@Body() createVoteDto: CreateVoteDto): Promise<Vote> {
+    return this.votesService.create(createVoteDto);
+  }
+}
