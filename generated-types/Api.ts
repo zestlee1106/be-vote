31cf052237df9e9e61a8ed108f9afd5465e6067f
이영우 <zestlee1106@gmail.com>
2024-07-18 11:16:00 +0900

    add: typescript generate 추가
    - 프론트에서 사용하기 편하도록 스웨거의 모든 json 을 타입으로 변환하는 스크립트를 짬

---
+++ generated-types/Api.ts
@@ -0,0 +1,390 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export interface CreateVoteDto {
+  /**
+   * 투표의 제목
+   * @example "투표 제목"
+   */
+  title: string;
+  /**
+   * 투표에 대한 설명 (선택 사항)
+   * @example "투표 설명"
+   */
+  description: string;
+  /**
+   * 투표 옵션의 목록
+   * @example ["옵션 1","옵션 2"]
+   */
+  options: string[];
+  /**
+   * 투표 시작 날짜
+   * @format date-time
+   * @example "2024-07-01T00:00:00.000Z"
+   */
+  startDate: string;
+  /**
+   * 투표 종료 날짜
+   * @format date-time
+   * @example "2024-07-15T00:00:00.000Z"
+   */
+  endDate: string;
+  /**
+   * 투표 결과를 저장하는 객체
+   * @example {"Option 1":10,"Option 2":5}
+   */
+  results: Record<string, number>;
+}
+
+export type ObjectId = object;
+
+export interface Vote {
+  /**
+   * The id of the vote
+   * @example "669523724633b96231437dcb"
+   */
+  _id: ObjectId;
+  /**
+   * The title of the vote
+   * @example "Vote title"
+   */
+  title: string;
+  /**
+   * The description of the vote
+   * @example "Vote description"
+   */
+  description: string;
+  /**
+   * The options for the vote
+   * @example ["Option 1","Option 2"]
+   */
+  options: string[];
+  /**
+   * The start date of the vote
+   * @format date-time
+   * @example "2024-07-01T00:00:00.000Z"
+   */
+  startDate: string;
+  /**
+   * The end date of the vote
+   * @format date-time
+   * @example "2024-07-15T00:00:00.000Z"
+   */
+  endDate: string;
+  /**
+   * The creation date of the vote
+   * @format date-time
+   * @example "2024-07-01T00:00:00.000Z"
+   */
+  createAt: string;
+  /**
+   * The last update date of the vote
+   * @format date-time
+   * @example "2024-07-01T00:00:00.000Z"
+   */
+  updatedAt: string;
+  /**
+   * The IP addresses of voters
+   * @example ["192.168.0.1"]
+   */
+  votedIps: string[];
+  /**
+   * The IP address of the vote creator
+   * @example "192.168.0.1"
+   */
+  creatorIp: string;
+  /**
+   * The UUID of the vote creator
+   * @example "5cc8a093-85fa-4da0-9111-313b53e4b924"
+   */
+  creatorUuid: string;
+  /**
+   * The UUIDs of voters
+   * @example ["uuid1","uuid2"]
+   */
+  votedCookieIds: string[];
+  /**
+   * The results of the vote
+   * @example {"Option 1":10,"Option 2":5}
+   */
+  results: object;
+}
+
+export type QueryParamsType = Record<string | number, any>;
+export type ResponseFormat = keyof Omit<Body, 'body' | 'bodyUsed'>;
+
+export interface FullRequestParams extends Omit<RequestInit, 'body'> {
+  /** set parameter to `true` for call `securityWorker` for this request */
+  secure?: boolean;
+  /** request path */
+  path: string;
+  /** content type of request body */
+  type?: ContentType;
+  /** query params */
+  query?: QueryParamsType;
+  /** format of response (i.e. response.json() -> format: "json") */
+  format?: ResponseFormat;
+  /** request body */
+  body?: unknown;
+  /** base url */
+  baseUrl?: string;
+  /** request cancellation token */
+  cancelToken?: CancelToken;
+}
+
+export type RequestParams = Omit<FullRequestParams, 'body' | 'method' | 'query' | 'path'>;
+
+export interface ApiConfig<SecurityDataType = unknown> {
+  baseUrl?: string;
+  baseApiParams?: Omit<RequestParams, 'baseUrl' | 'cancelToken' | 'signal'>;
+  securityWorker?: (securityData: SecurityDataType | null) => Promise<RequestParams | void> | RequestParams | void;
+  customFetch?: typeof fetch;
+}
+
+export interface HttpResponse<D extends unknown, E extends unknown = unknown> extends Response {
+  data: D;
+  error: E;
+}
+
+type CancelToken = Symbol | string | number;
+
+export enum ContentType {
+  Json = 'application/json',
+  FormData = 'multipart/form-data',
+  UrlEncoded = 'application/x-www-form-urlencoded',
+  Text = 'text/plain',
+}
+
+export class HttpClient<SecurityDataType = unknown> {
+  public baseUrl: string = '';
+  private securityData: SecurityDataType | null = null;
+  private securityWorker?: ApiConfig<SecurityDataType>['securityWorker'];
+  private abortControllers = new Map<CancelToken, AbortController>();
+  private customFetch = (...fetchParams: Parameters<typeof fetch>) => fetch(...fetchParams);
+
+  private baseApiParams: RequestParams = {
+    credentials: 'same-origin',
+    headers: {},
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+  };
+
+  constructor(apiConfig: ApiConfig<SecurityDataType> = {}) {
+    Object.assign(this, apiConfig);
+  }
+
+  public setSecurityData = (data: SecurityDataType | null) => {
+    this.securityData = data;
+  };
+
+  protected encodeQueryParam(key: string, value: any) {
+    const encodedKey = encodeURIComponent(key);
+    return `${encodedKey}=${encodeURIComponent(typeof value === 'number' ? value : `${value}`)}`;
+  }
+
+  protected addQueryParam(query: QueryParamsType, key: string) {
+    return this.encodeQueryParam(key, query[key]);
+  }
+
+  protected addArrayQueryParam(query: QueryParamsType, key: string) {
+    const value = query[key];
+    return value.map((v: any) => this.encodeQueryParam(key, v)).join('&');
+  }
+
+  protected toQueryString(rawQuery?: QueryParamsType): string {
+    const query = rawQuery || {};
+    const keys = Object.keys(query).filter((key) => 'undefined' !== typeof query[key]);
+    return keys
+      .map((key) => (Array.isArray(query[key]) ? this.addArrayQueryParam(query, key) : this.addQueryParam(query, key)))
+      .join('&');
+  }
+
+  protected addQueryParams(rawQuery?: QueryParamsType): string {
+    const queryString = this.toQueryString(rawQuery);
+    return queryString ? `?${queryString}` : '';
+  }
+
+  private contentFormatters: Record<ContentType, (input: any) => any> = {
+    [ContentType.Json]: (input: any) =>
+      input !== null && (typeof input === 'object' || typeof input === 'string') ? JSON.stringify(input) : input,
+    [ContentType.Text]: (input: any) => (input !== null && typeof input !== 'string' ? JSON.stringify(input) : input),
+    [ContentType.FormData]: (input: FormData) =>
+      (Array.from(input.keys()) || []).reduce((formData, key) => {
+        const property = input.get(key);
+        formData.append(
+          key,
+          property instanceof Blob
+            ? property
+            : typeof property === 'object' && property !== null
+              ? JSON.stringify(property)
+              : `${property}`,
+        );
+        return formData;
+      }, new FormData()),
+    [ContentType.UrlEncoded]: (input: any) => this.toQueryString(input),
+  };
+
+  protected mergeRequestParams(params1: RequestParams, params2?: RequestParams): RequestParams {
+    return {
+      ...this.baseApiParams,
+      ...params1,
+      ...(params2 || {}),
+      headers: {
+        ...(this.baseApiParams.headers || {}),
+        ...(params1.headers || {}),
+        ...((params2 && params2.headers) || {}),
+      },
+    };
+  }
+
+  protected createAbortSignal = (cancelToken: CancelToken): AbortSignal | undefined => {
+    if (this.abortControllers.has(cancelToken)) {
+      const abortController = this.abortControllers.get(cancelToken);
+      if (abortController) {
+        return abortController.signal;
+      }
+      return void 0;
+    }
+
+    const abortController = new AbortController();
+    this.abortControllers.set(cancelToken, abortController);
+    return abortController.signal;
+  };
+
+  public abortRequest = (cancelToken: CancelToken) => {
+    const abortController = this.abortControllers.get(cancelToken);
+
+    if (abortController) {
+      abortController.abort();
+      this.abortControllers.delete(cancelToken);
+    }
+  };
+
+  public request = async <T = any, E = any>({
+    body,
+    secure,
+    path,
+    type,
+    query,
+    format,
+    baseUrl,
+    cancelToken,
+    ...params
+  }: FullRequestParams): Promise<HttpResponse<T, E>> => {
+    const secureParams =
+      ((typeof secure === 'boolean' ? secure : this.baseApiParams.secure) &&
+        this.securityWorker &&
+        (await this.securityWorker(this.securityData))) ||
+      {};
+    const requestParams = this.mergeRequestParams(params, secureParams);
+    const queryString = query && this.toQueryString(query);
+    const payloadFormatter = this.contentFormatters[type || ContentType.Json];
+    const responseFormat = format || requestParams.format;
+
+    return this.customFetch(`${baseUrl || this.baseUrl || ''}${path}${queryString ? `?${queryString}` : ''}`, {
+      ...requestParams,
+      headers: {
+        ...(requestParams.headers || {}),
+        ...(type && type !== ContentType.FormData ? { 'Content-Type': type } : {}),
+      },
+      signal: (cancelToken ? this.createAbortSignal(cancelToken) : requestParams.signal) || null,
+      body: typeof body === 'undefined' || body === null ? null : payloadFormatter(body),
+    }).then(async (response) => {
+      const r = response.clone() as HttpResponse<T, E>;
+      r.data = null as unknown as T;
+      r.error = null as unknown as E;
+
+      const data = !responseFormat
+        ? r
+        : await response[responseFormat]()
+            .then((data) => {
+              if (r.ok) {
+                r.data = data;
+              } else {
+                r.error = data;
+              }
+              return r;
+            })
+            .catch((e) => {
+              r.error = e;
+              return r;
+            });
+
+      if (cancelToken) {
+        this.abortControllers.delete(cancelToken);
+      }
+
+      if (!response.ok) throw data;
+      return data;
+    });
+  };
+}
+
+/**
+ * @title 투표 시스템 스웨거
+ * @version 1.0
+ * @contact
+ *
+ * 스웨거 입니다
+ */
+export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDataType> {
+  votes = {
+    /**
+     * No description
+     *
+     * @tags votes
+     * @name VotesControllerCreate
+     * @summary 새로운 투표 생성
+     * @request POST:/votes
+     */
+    votesControllerCreate: (data: CreateVoteDto, params: RequestParams = {}) =>
+      this.request<Vote, any>({
+        path: `/votes`,
+        method: 'POST',
+        body: data,
+        type: ContentType.Json,
+        format: 'json',
+        ...params,
+      }),
+
+    /**
+     * No description
+     *
+     * @tags votes
+     * @name VotesControllerGetAll
+     * @summary 모든 투표 가져오기
+     * @request GET:/votes
+     */
+    votesControllerGetAll: (params: RequestParams = {}) =>
+      this.request<Vote[], any>({
+        path: `/votes`,
+        method: 'GET',
+        format: 'json',
+        ...params,
+      }),
+
+    /**
+     * No description
+     *
+     * @tags votes
+     * @name VotesControllerGetOne
+     * @summary id 로 투표를 가져오기
+     * @request GET:/votes/{id}
+     */
+    votesControllerGetOne: (id: string, params: RequestParams = {}) =>
+      this.request<Vote, void>({
+        path: `/votes/${id}`,
+        method: 'GET',
+        format: 'json',
+        ...params,
+      }),
+  };
+}
